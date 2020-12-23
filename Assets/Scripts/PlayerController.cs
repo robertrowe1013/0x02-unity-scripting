ba282a2a3b41e2b96823d49fdabaeed2357cbfdf
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-23 10:43:51 -0600

    task 2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerBody;
     private Vector3 inputVector;
-    public float speed = 10f;
+    public float speed = 100f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +15,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         inputVector = new Vector3(Input.GetAxisRaw("Horizontal") * speed, 0, Input.GetAxisRaw("Vertical") * speed);
         playerBody.velocity = inputVector;
