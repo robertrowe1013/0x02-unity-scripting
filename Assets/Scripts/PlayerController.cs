3793a9005a473c451aa8241a59cd889f1da8b557
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-23 13:45:19 -0600

    task 4

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerBody;
     private Vector3 inputVector;
     public float speed = 100f;
+    private int score = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -19,5 +20,17 @@ public class PlayerController : MonoBehaviour
     {
         inputVector = new Vector3(Input.GetAxisRaw("Horizontal") * speed, 0, Input.GetAxisRaw("Vertical") * speed);
         playerBody.velocity = inputVector;
+    }
+
+    // scoring
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Pickup")
+        {
+            score++;
+            string str = "Score: " + score;
+            Debug.Log(str);
+            Object.Destroy(other.gameObject);
+        }
     }
 }
